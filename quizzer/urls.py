d6d5cc874c8fc3aa6272aeaa59321e331da04a89
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-03-01 13:09:35 +0100

    Add separate healthcheck route

--- quizzer/urls.py
+++ quizzer/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path("healthz", lambda request: HttpResponse("ok")),
     path("", lambda request: redirect("quiz:start")),
     path("quiz/", include("quizzer.snippetz.urls")),
     path("admin/", admin.site.urls),
