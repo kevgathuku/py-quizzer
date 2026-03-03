658bbeecfdd77cb1c7489977828570e542a6719c
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-03-03 19:58:05 +0100

    Remove unused AUTH_PASSWORD_VALIDATORS
    App has no user authentication — password validators are inert config.

--- quizzer/settings.py
+++ quizzer/settings.py
@@ -93,25 +93,6 @@ else:
     }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
